1d3b4adfde541566849a8cc6e41d6177bc64cad2
Alex <sashakasyanova54@gmail.com>
2026-03-04 23:07:14 +0800

    made a calculate_tax func and test for it

--- testing/math_testing.py
+++ testing/math_testing.py
@@ -1,6 +1,8 @@
 from my_math import (
 add,
 add_with_bug,
+calculate_tax_with_bug,
+calculate_tax
 )
 
 def test_addition():
@@ -37,9 +39,34 @@ def test_commutative():
     assert add(-2, 2) == 0
     print("Commutative is true")
 
+#переходим к тесту новой функ
+def calculate_tax_with_bug_check():
+    # типами ограничили случаи которые можем посмотреть
+
+    try:
+        assert calculate_tax_with_bug(1000) == 150.0
+        assert calculate_tax_with_bug(100) == 15.0
+        assert calculate_tax_with_bug(10) == 1.5
+        assert calculate_tax_with_bug(1) == 0.15
+        assert calculate_tax_with_bug(243) == 36.45
+    except:
+        print("Tax Calculation doesnt work")
+
+def test_tax_calculation():
+    assert calculate_tax(1000) == 150.0
+    assert calculate_tax(100) == 15.0
+    assert calculate_tax(10) == 1.5
+    assert calculate_tax(1) == 0.15
+    assert calculate_tax(24.5) == 3.67
+    print("Tax Calculation works")
+
+#На самом деле нет, надо как то сотые копейки посчитать, прям щас думать не хочу
+
 if __name__ == "__main__":
     test_addition()
     test_with_bug()
     # test_dublicate()
     test_reasonable()
     test_commutative()
+    calculate_tax_with_bug_check()
+    test_tax_calculation()
